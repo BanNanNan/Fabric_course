99e5bd4bdf05bc41212838dd4fb5b05b4af691d4
AnHyeonUng <frukp098@naver.com>
2022-09-09 16:45:00 +0900

    added comment

--- src/main/java/net/bannannan/mccourse/MCCourseMod.java
+++ src/main/java/net/bannannan/mccourse/MCCourseMod.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 public class MCCourseMod implements ModInitializer {
 	public static final String MOD_ID = "mccourse";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	//Added comment
 
 	@Override
 	public void onInitialize() {
